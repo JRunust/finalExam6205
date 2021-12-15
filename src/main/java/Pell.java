50a2a75595fbfbf44ad4aa8d5d7341e313bf4f4b
hr574 <hr574177@gmail.com>
2021-12-15 15:25:05 -0500

    Initial commit

---
+++ src/main/java/Pell.java
@@ -0,0 +1,17 @@
+public class Pell {
+    public Pell() {
+    }
+    public long get(int n) {
+        if (n < 0) throw new UnsupportedOperationException("Pell.get is not supported for negative n");
+        if(n <= 1){
+            return n;
+        }
+        long[] arr = new long[n + 1];
+        arr[0] = 0l;
+        arr[1] = 1l;
+        for(int i = 2;i <= n;i++){
+            arr[i] = 2 * arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+     }
+}
